3e1eaefe976efd5f902b77ab68535cb89ee2ab4f
Abah Faqih <rasyidperkim@gmail.com>
2021-08-14 20:31:45 +0800

    menambahkan heading

--- resources/views/pages/space/home.blade.php
+++ resources/views/pages/space/home.blade.php
@@ -8,14 +8,9 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    {{ __('Selamat Datang di Codespace') }}
                 </div>
+                <h2>Silahkan Klik Space</h2>
             </div>
         </div>
     </div>
